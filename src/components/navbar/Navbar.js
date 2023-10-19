5d7173787352736b520fec528db01d03b53b28de
omkar <omkarjarad2019@gmail.com>
2023-10-20 02:42:33 +0530

    navbar completed

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,35 @@
+import { Fragment, useState } from "react";
+// import { Dialog, Popover, Transition } from "@headlessui/react";
+// import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <div className="navbar flex-col">
+      <nav class="navbar bg-body-tertiary d-flex">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            <h1 class="d-inline-block align-text-top text-white mx-4 my-4 font-bold font-sans text-3xl">
+              STOCK EXCHANGER
+            </h1>
+          </a>
+        </div>
+        <div class="btn_div">
+          <button class="btn btn-sm my-4 mx-4">Get Started</button>
+        </div>
+      </nav>
+      <div className="hello_sec">
+        <h1 className="text-white mx-4 my-10 text-5xl font-sans font-bold">
+          Revamp Your Trading Experience
+        </h1>
+        <p className="p font-sans font-normal">
+          Welcome to Equilink, the go-to destination for all your stock exchange
+          need
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
+// https://dribbble.com/shots/21918621-Equilink-Landing-page-for-stock-exchange-platform
